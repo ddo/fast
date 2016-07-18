cd978a0251d02954785e4bc49a21450a35743a80
ddo <joeddo89@gmail.com>
2016-07-18 22:36:15 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # fast
-Minimal zero-dependency utility for testing your internet speed from terminal
+> Minimal zero-dependency utility for testing your internet speed from terminal
+
+*Powered by Fast.com - Netflix*
+
+[![asciicast](https://asciinema.org/a/80106.png)](https://asciinema.org/a/80106)
+
+## Download to your bin folder
+
+* direct download the binary for your os here https://github.com/ddo/fast/releases
+
+* or from terminal
+
+replace the download link with your os one
+
+*below is ubuntu 64 bit example*
+
+### with curl
+
+```sh
+curl -L https://github.com/ddo/fast/releases/download/v0.0.1/fast_linux_amd64 -o fast
+```
+
+### or wget
+
+```sh
+wget https://github.com/ddo/fast/releases/download/v0.0.1/fast_linux_amd64 -O fast
+```
+
+## Then chmod
+
+```sh
+chmod +x fast
+```
+
+## Usage
+
+* run direct from download folder
+
+```sh
+./fast
+```
+
+* if you download to your bin folder
+
+```
+fast
+```
+
+## For golang user
+
+golang user can install from the source code
+
+```sh
+go get github.com/ddo/fast
+```
+
+## Bug
+
+for bug report just open new issue
---
+++ fast.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ddo/go-fast"
+	"github.com/ddo/go-spin"
+)
+
+const (
+	ERR = "\r internet error. please try again"
+)
+
+func main() {
+	status := ""
+	spinner := spin.New("")
+
+	// output
+	ticker := time.NewTicker(100 * time.Millisecond)
+
+	go func() {
+		for range ticker.C {
+			fmt.Printf("\r %s  %s", spinner.Spin(), status)
+		}
+	}()
+	// output
+
+	fastCom := fast.New()
+
+	// init
+	err := fastCom.Init()
+
+	if err != nil {
+		fmt.Println(ERR)
+		return
+	}
+
+	status = "connecting   "
+
+	// get urls
+	urls, err := fastCom.GetUrls()
+
+	if err != nil {
+		fmt.Println(ERR)
+		return
+	}
+
+	status = "loading      "
+
+	// measure
+	KbpsChan := make(chan float64)
+
+	go func() {
+		for Kbps := range KbpsChan {
+			status = format(Kbps) + "    "
+		}
+
+		fmt.Printf("\r -> %s\n", status)
+	}()
+
+	err = fastCom.Measure(urls, KbpsChan)
+
+	if err != nil {
+		fmt.Println(ERR)
+	}
+
+	ticker.Stop()
+	return
+}
+
+func format(Kbps float64) string {
+	unit := "Kbps"
+	f := "%.f %s"
+
+	if Kbps > 1000 {
+		unit = "Mbps"
+		Kbps /= 1000 // Mbps
+		f = "%.2f %s"
+	}
+
+	return fmt.Sprintf(f, Kbps, unit)
+}
