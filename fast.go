f422e5202040555ecdf5b90df7979e437768d91f
fuele <37090676+fuele@users.noreply.github.com>
2021-11-24 04:19:27 -0600

    Make the output machine readable (#18)
    * Use go module instead of gopkg, add gitignore for binary
    
    * Added flags which force the output into the desired metric
    
    * Adding non-zero return codes for when the program fails
    
    * Added a silent switch to surpress the status message to make it more machine readable.
    
    * Updated the README with additional arguments
    
    * Ran fmt
    
    Co-authored-by: Mike Hix <mhix@newrelic.com>

--- fast.go
+++ fast.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/ddo/go-fast"
@@ -9,17 +11,31 @@ import (
 )
 
 func main() {
+	var kb, mb, gb, silent bool
+	flag.BoolVar(&kb, "k", false, "Format output in Kbps")
+	flag.BoolVar(&mb, "m", false, "Format output in Mbps")
+	flag.BoolVar(&gb, "g", false, "Format output in Gbps")
+	flag.BoolVar(&silent, "silent", false, "Surpress all output except for the final result")
+
+	flag.Parse()
+
+	if kb && (mb || gb) || (mb && kb) {
+		fmt.Println("You may have at most one formating switch. Choose either -k, -m, or -g")
+		os.Exit(-1)
+	}
+
 	status := ""
 	spinner := spin.New("")
 
 	// output
 	ticker := time.NewTicker(100 * time.Millisecond)
-
-	go func() {
-		for range ticker.C {
-			fmt.Printf("%c[2K %s  %s\r", 27, spinner.Spin(), status)
-		}
-	}()
+	if !silent {
+		go func() {
+			for range ticker.C {
+				fmt.Printf("%c[2K %s  %s\r", 27, spinner.Spin(), status)
+			}
+		}()
+	}
 	// output
 
 	fastCom := fast.New()
@@ -28,7 +44,7 @@ func main() {
 	err := fastCom.Init()
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	status = "connecting"
@@ -37,7 +53,7 @@ func main() {
 	urls, err := fastCom.GetUrls()
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	status = "loading"
@@ -46,36 +62,72 @@ func main() {
 	KbpsChan := make(chan float64)
 
 	go func() {
+		var value, units string
 		for Kbps := range KbpsChan {
-			status = format(Kbps)
-		}
+			value, units = format(Kbps, kb, mb, gb)
+			// don't print the units of measurement if explicitly asked for
+			if kb || mb || gb {
+				status = fmt.Sprintf("%s", value)
+			} else {
+				status = fmt.Sprintf("%s %s", value, units)
+			}
 
-		fmt.Printf("\r%c[2K -> %s\n", 27, status)
+		}
+		if silent {
+			fmt.Printf("%s\n", status)
+		} else {
+			fmt.Printf("\r%c[2K -> %s\n", 27, status)
+		}
 	}()
 
 	err = fastCom.Measure(urls, KbpsChan)
+	ticker.Stop()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	ticker.Stop()
 	return
 }
 
-func format(Kbps float64) string {
+func formatGbps(Kbps float64) (string, string, float64) {
+	f := "%.2f"
+	unit := "Gbps"
+	value := Kbps / 1000000
+	return f, unit, value
+}
+func formatMbps(Kbps float64) (string, string, float64) {
+	f := "%.2f"
+	unit := "Mbps"
+	value := Kbps / 1000
+	return f, unit, value
+}
+func formatKbps(Kbps float64) (string, string, float64) {
+	f := "%.f"
 	unit := "Kbps"
-	f := "%.f %s"
+	value := Kbps
+	return f, unit, value
+}
 
-	if Kbps > 1000000 { // Gbps
-		f = "%.2f %s"
-		unit = "Gbps"
-		Kbps /= 1000000
+func format(Kbps float64, kb bool, mb bool, gb bool) (string, string) {
+	var value float64
+	var unit string
+	var f string
 
+	if kb {
+		f, unit, value = formatKbps(Kbps)
+	} else if mb {
+		f, unit, value = formatMbps(Kbps)
+	} else if gb {
+		f, unit, value = formatGbps(Kbps)
+	} else if Kbps > 1000000 { // Gbps
+		f, unit, value = formatGbps(Kbps)
 	} else if Kbps > 1000 { // Mbps
-		f = "%.2f %s"
-		unit = "Mbps"
-		Kbps /= 1000
+		f, unit, value = formatMbps(Kbps)
+	} else {
+		f, unit, value = formatKbps(Kbps)
 	}
 
-	return fmt.Sprintf(f, Kbps, unit)
+	strValue := fmt.Sprintf(f, value)
+	return strValue, unit
 }
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/ddo/fast
+
+require (
+	github.com/ddo/go-fast v0.0.0-20180221183904-3a2f747dc782
+	github.com/ddo/go-spin v0.0.0-20160718105357-ab0a40e623c0
+	github.com/ddo/pick-json v0.0.0-20170207095303-c8760e09e0fe
+	github.com/ddo/rq v0.0.0-20180104074423-d8ad0e0faf64
+	golang.org/x/net v0.0.0-20180218175443-cbe0f9307d01
+	gopkg.in/ddo/go-dlog.v2 v2.1.0
+	gopkg.in/ddo/pick.v1 v1.2.2
+)
